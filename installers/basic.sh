f20fb9279fe9cbc3d6c40564c8a875a19e21f069
Rodrigo Gonçalves <rodrigogoncalveess@gmail.com>
2021-05-17 08:46:32 -0300

    add qbittorrent to basic installs

--- installers/basic.sh
+++ installers/basic.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-APPS="apt-transport-https ca-certificates cheese chrome-gnome-shell curl dconf-cli dirmngr dotnet-sdk-5.0 ffmpegthumbnailer flameshot git gnome-mpv gnome-shell-extensions gnome-tweak-tool gnupg gnupg-agent lutris mono-complete obs-studio openjdk-11-jdk papirus-icon-theme peek playonlinux preload python3-pip software-properties-common steam transmission ubuntu-restricted-extras unzip viewnior vim zram-config zsh zsh-syntax-highlighting"
+APPS="apt-transport-https ca-certificates cheese chrome-gnome-shell curl dconf-cli dirmngr dotnet-sdk-5.0 ffmpegthumbnailer flameshot git gnome-mpv gnome-shell-extensions gnome-tweak-tool gnupg gnupg-agent lutris mono-complete obs-studio openjdk-11-jdk papirus-icon-theme peek playonlinux preload python3-pip qbittorrent software-properties-common steam transmission ubuntu-restricted-extras unzip viewnior vim zram-config zsh zsh-syntax-highlighting"
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
